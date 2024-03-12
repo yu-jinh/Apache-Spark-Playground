a72b9e5e23a40db5786797a980ca0d1d0e3daec6
hong <yjh849625800@gmail.com>
2024-03-12 10:26:27 -0400

    first script for word count

---
+++ word_count.py
@@ -0,0 +1,14 @@
+from pyspark import SparkContext, SparkConf
+
+if __name__ == "__main__":
+    conf = SparkConf().setAppName("word count").setMaster("local[3]")
+    sc = SparkContext(conf = conf)
+    
+    lines = sc.textFile("data/word_count.text")
+    
+    words = lines.flatMap(lambda line: line.split(" "))
+    
+    wordCounts = words.countByValue()
+    
+    for word, count in wordCounts.items():
+        print("{} : {}".format(word, count))
