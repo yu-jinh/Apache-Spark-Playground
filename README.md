483942893c02018ebc7ee35c5d7210f0a6cd6915
hong <yjh849625800@gmail.com>
2024-03-29 09:10:11 -0400

    code update

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Apache-Spark-Playground
-This is an experimential project for apache spark learning. It will cover several basic operation.
+
+This is an experimental project for Apache Spark learning. It will cover several basic operations.
+
+# Reference
+
+Thanks for the amazing tutorial on https://www.youtube.com/watch?v=W__Jk83gOyo&list=PL0hSJrxggIQr6wA8buIn1Yxu810ugGed-&index=5
+
+The corresponding GitHub repository: https://github.com/jleetutorial/python-spark-tutorial
--- rdd/WordCount.py
+++ rdd/WordCount.py
@@ -3,12 +3,15 @@ from pyspark import SparkContext, SparkConf
 if __name__ == "__main__":
     conf = SparkConf().setAppName("word count").setMaster("local[3]")
     sc = SparkContext(conf = conf)
-    
-    lines = sc.textFile("data/word_count.text")
-    
+    sc.setLogLevel("ERROR")
+    lines = sc.textFile("in/word_count.text")
+    print(lines)
     words = lines.flatMap(lambda line: line.split(" "))
-    
+    print(words)
     wordCounts = words.countByValue()
-    
+    print()
+    print(wordCounts)
     for word, count in wordCounts.items():
         print("{} : {}".format(word, count))
+
+
